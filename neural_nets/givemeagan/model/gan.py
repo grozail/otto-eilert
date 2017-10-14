57a303812b5082970212752b7927f972b675befd
Ilya <grozail@yandex.ru>
2017-10-15 01:49:50 +0300

    first descriminator and .idea ignore

--- neural_nets/givemeagan/model/gan.py
+++ neural_nets/givemeagan/model/gan.py
@@ -34,6 +34,24 @@ class Generator(tnn.Module):
 
 
 class Descriminator(tnn.Module):
-    def __init__(self):
+    def __init__(self, n_features):
         super(Descriminator, self).__init__()
-        
+        self.main_net = tnn.Sequential(
+            # input signals 1 x 32 x 32
+            tnn.Conv2d(1, n_features, 4, 2, 1, bias=False),
+            tnn.LeakyReLU(0.1, True),
+            # n_features x 16 x 16
+            tnn.Conv2d(n_features, n_features * 2, 4, 2, 1, bias=False),
+            tnn.BatchNorm2d(n_features * 2),
+            tnn.LeakyReLU(0.1, True),
+            # n_features * 2 x 8 x 8
+            tnn.Conv2d(n_features * 2, n_features * 4, 4, 2, 1, bias=False),
+            tnn.BatchNorm2d(n_features * 4),
+            tnn.LeakyReLU(0.1, True),
+            # n_features * 4 x 4 x 4
+            tnn.Conv2d(n_features * 4, 1, 4, 1, 0, bias=False),
+            tnn.Sigmoid(),
+        )
+    
+    def forward(self, inp):
+        return self.main_net(inp)
