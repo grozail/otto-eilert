4b119c5cbce9331a7bea8ef3a6bb12588376f91d
Ilya <grozail@yandex.ru>
2017-12-04 17:11:34 +0300

    delete unused

--- neural_nets/givemeagan/model/cyrilic_dataset.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data.dataset import Dataset
-from torchvision import transforms, datasets
-import pandas as pd
-import os
-
-data_transforms = transforms.Compose([
-    transforms.ToTensor()
-])
-
